d781c1b5e94047afe4924ed399554d6ca2e4c891
scraple1234567890-lab <scraple1234567890@gmail.com>
2026-02-02 14:09:57 -0600

    jjnkjnnnjnjn

--- data/searchIndex-data.js
+++
@@ -1,3 +0,0 @@
-// Fallback for universal search when opened via file://
-// Use this file for extra entries that aren't already in map-locations.json, timeline.json, or characters.json.
-window.SEARCH_INDEX_DATA = [];
